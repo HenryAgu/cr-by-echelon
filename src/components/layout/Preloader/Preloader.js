6141542b72c637bb1921cd3465dfd4983998bc7b
Henry Agu <74037448+HenryAgu@users.noreply.github.com>
2023-03-19 12:09:44 +0100

    Add files via upload

--- src/components/layout/Preloader/Preloader.js
+++ src/components/layout/Preloader/Preloader.js
@@ -1,16 +1,23 @@
-import React from 'react';
+// import React, { useEffect, useState } from "react";
 
 // stylesheet
-import './Preloader.css';
+import "./Preloader.css";
+
+// animate.css
+import "animate.css";
 
 // image
-import preloadImage from '../../../img/single-logo.png';
+import preloadImage from "../../../img/single-logo.png";
 const Preloader = () => {
   return (
-    <div className='preload'>
-            <img src={preloadImage} alt="" />
+    <div className="preload">
+      <img
+        className="animate__animated animate__pulse animate__infinite-infinite"
+        src={preloadImage}
+        alt=""
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Preloader
+export default Preloader;
